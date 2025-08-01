33da201ced98855ae60ac5089c019ae78da17e3b
Daniel McKee <danielm.mckee@gmail.com>
2025-08-02 00:15:59 +0100

    Attempt to store price

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on) {
       on('task', {
         async savePriceToFirebase(data) {
+          const admin = require('firebase-admin');
+
           console.log("DATA PRINT", data);
           if (!process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
             throw new Error("FIREBASE_SERVICE_ACCOUNT_JSON is not set");
